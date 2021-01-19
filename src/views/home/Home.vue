ba34d0b723f2a8c54e4339e4ebebe053064920c7
diangen <diangen@qq.com>
2021-01-19 09:11:04 +0800

    ''

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,16 +3,19 @@
     <nav-bar class="home-bar">
       <div slot="center">购物街</div>
     </nav-bar>
+    <recommend-view :recommends="recommedns"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/navbar.vue";
 import { getHomeMultidata } from "network/home";
+import RecommendView from './childComps/RecommendView'
 export default {
   name: "Home",
   components: {
     NavBar,
+    RecommendView
   },
   data() {
     return {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommedns" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RecommendView",
+  props: {
+    recommedns: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style  scoped>
+.recommend {
+  display: flex;
+  text-align: center;
+  width: 100%;
+  font-size: 12px;
+  padding: 10px 0px 20px;
+  border-bottom:8px solid #eeeeee ;
+}
+.recommend-item {
+  flex: 1;
+}
+.recommend-item img {
+  width: 65px;
+  height: 65px;
+  margin-bottom: 2px;
+}
+</style>
